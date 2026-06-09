7d8c163823b6644d0fcd222a1fd01291adc05abf
DevFullFelps <devfullfelps@gmail.com>
2026-06-09 08:51:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const URL_API = "https://prototipo-cardapio.onrender.com/produtos";
 
 async function carregarCardapio() {
+    const container = document.getElementById("container-cardapio");
+    container.innerHTML = `<p id="loading">Carregando o cardápio... (O servidor gratuito pode levar até 2 minutos para iniciar)</p>`;
     const resposta = await fetch(URL_API);
     const produtos = await resposta.json();
-    const container = document.getElementById("container-cardapio");
-    container.innerHTML = "";
+    container.innerHTML = ""
     produtos.forEach(produto => {
         const cardProduto = `
             <div style="border: 1px solid #ccc; padding: 10px; margin: 10px; border-radius: 8px;">
